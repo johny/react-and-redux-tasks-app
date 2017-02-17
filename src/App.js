54d5ffe4dd25bcabbf5068628b8facc9f010b6a3
Jan Kraus <kraus.jan@gmail.com>
2017-02-17 20:20:57 +0000

    Initial app, from React Create App

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App__headline">Tasks</div>
+        <div className="App__content">
+          App content goes here!
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
